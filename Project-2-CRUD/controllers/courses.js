e58785bc6ba7fc11da67d92bd95932de0479b3ea
mitchgemma <mitchgemma@gmail.com>
2022-03-15 11:15:57 -0400

    added courses controller and initial index.liquid for all courses

---
+++ Project-2-CRUD/controllers/courses.js
@@ -0,0 +1,64 @@
+/////////////////////////////////////////////////
+// import dependencies
+/////////////////////////////////////////////////
+const express = require('express')
+const Courses = require('../models/courses')
+
+/////////////////////////////////////////////////
+// import dependencies
+/////////////////////////////////////////////////
+const router = express.Router()
+
+////////////////////////////////////////////
+// Router Middleware
+////////////////////////////////////////////
+// create some middleware to protect these routes
+// Authorization middleware
+router.use((req, res, next) => {
+	// checking the loggedin boolean of our session
+	if (req.session.loggedIn) {
+		// if they're logged in, go to the next thing(thats the controller)
+		next()
+	} else {
+		// if they're not logged in, send them to the login page
+		res.redirect('/user/login')
+	}
+})
+
+////////////////////////////////////////////
+// Router Middleware
+////////////////////////////////////////////
+router.use((req, res, next) => {
+	// checking the loggedIn boolean of our session
+	if (req.session.loggedIn) {
+		// if they're logged in, go to the next thing(thats the controller)
+		next()
+	} else {
+		// if they're not logged in, send them to the login page
+		res.redirect('/auth/login')
+	}
+})
+
+////////////////////////////////////////////
+// Routes
+////////////////////////////////////////////
+
+// index ALL courses route
+router.get('/', (req, res) => {
+	// find the fruits
+	Courses.find({})
+		// then render a template AFTER they're found
+		.then((courses) => {
+			const username = req.session.username
+			const loggedIn = req.session.loggedIn
+			// console.log(fruits)
+			res.render('courses/index', { fruits, username, loggedIn })
+        })
+        // redirect to the error page if there is one
+		.catch(error => {
+			res.redirect(`/error?error=${error}`)
+		})
+})
+
+// Export the Router
+module.exports = router
