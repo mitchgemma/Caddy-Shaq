371ab7e56b70323d62f1e26ba3985313719432d8
mitchgemma <mitchgemma@gmail.com>
2022-03-15 15:42:25 -0400

    fixed bug with router not working & added index route and start index page

--- Project-2-CRUD/utils/middleware.js
+++ Project-2-CRUD/utils/middleware.js
@@ -5,6 +5,7 @@ require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
 const methodOverride = require('method-override')
+const CourseRouter = require('../controllers/courses')
 const session = require('express-session')
 const MongoStore = require('connect-mongo')
 
